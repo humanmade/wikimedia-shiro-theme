bb6c9eaf0c881dedddbea68b567dc3ad06382de6
Pamela Ribeiro <contato@pamelanascimento.com>
2022-08-18 15:22:14 -0300

    Created styling files

--- assets/src/editor/blocks/accordion-item/index.js
+++ assets/src/editor/blocks/accordion-item/index.js
@@ -9,70 +9,73 @@ import { __ } from '@wordpress/i18n';
 
 import { InnerBlocks, RichText, useBlockProps } from '@wordpress/block-editor';
 
-export const
-	name = 'shiro/accordion-item',
-	settings = {
-		apiVersion: 2,
-		title: __( 'Accordion Item', 'shiro-admin' ),
-		icon: 'minus',
-		category: 'wikimedia',
-		supports: {
-			align: [ 'center', 'full' ],
-		},
+import './style.scss';
 
-		/**
-		 * Edit the block.
-		 */
-		 edit: ( {
-			attributes,
-			setAttributes,
-			setFocus,
-		} ) => {
-			const blockProps = useBlockProps(); // eslint-disable-line react-hooks/rules-of-hooks
 
-			return (
-				<div { ...blockProps }>
-					<div aria-expanded className="accordion-item">
-						<div className="accordion-item__title">
-							<RichText
-								className="accordion-item__title-text"
-								formattingControls={ [] }
-								placeholder={
-									'Add Accordion Title...'
-								}
-								tagName="h3"
-								value={ attributes.title }
-								onChange={ content => setAttributes( { title: content } ) }
-								onFocus={ setFocus }
-							/>
-						</div>
+export const name = 'shiro/accordion-item';
 
-						<div className="accordion-item__content">
-							<InnerBlocks/>
-						</div>
-					</div>
-				</div>
-			);
-		},
+export const settings = {
+	apiVersion: 2,
+	title: __( 'Accordion Item', 'shiro-admin' ),
+	icon: 'minus',
+	category: 'wikimedia',
+	supports: {
+		align: [ 'center', 'full' ],
+	},
 
-		/**
-		 * Save nothing, to allow for server-size rendering.
-		 */
-		 save: ( { attributes } ) => {
-			return (
-				<div aria-expanded={ false } className="accordion-item">
-					<button className="accordion-item__title">
-						<RichText.Content
+	/**
+	 * Edit the block.
+	 */
+		edit: ( {
+		attributes,
+		setAttributes,
+		setFocus,
+	} ) => {
+		const blockProps = useBlockProps(); // eslint-disable-line react-hooks/rules-of-hooks
+
+		return (
+			<div { ...blockProps }>
+				<div aria-expanded className="accordion-item">
+					<div className="accordion-item__title">
+						<RichText
 							className="accordion-item__title-text"
+							formattingControls={ [] }
+							placeholder={
+								'Add Accordion Title...'
+							}
 							tagName="h3"
 							value={ attributes.title }
-						/>
-					</button>
+							onChange={ content => setAttributes( { title: content } ) }
+							onFocus={ setFocus }
+						></RichText>
+					</div>
 
 					<div className="accordion-item__content">
-						<InnerBlocks.Content/>
+						<InnerBlocks/>
 					</div>
 				</div>
-			);
-		 }
-	};
+			</div>
+		);
+	},
+
+	/**
+	 * Save nothing, to allow for server-size rendering.
+	 */
+		save: ( { attributes } ) => {
+		return (
+			<div aria-expanded={ false } className="accordion-item">
+				<button className="accordion-item__title">
+					<RichText.Content
+						className="accordion-item__title-text"
+						tagName="h3"
+						value={ attributes.title }
+					/>
+				</button>
+
+				<div className="accordion-item__content">
+					<InnerBlocks.Content/>
+				</div>
+			</div>
+		);
+		}
+};
--- assets/src/editor/blocks/accordion/index.js
+++ assets/src/editor/blocks/accordion/index.js
@@ -9,34 +9,38 @@ import { __ } from '@wordpress/i18n';
 
 import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
-export const
-	name = 'shiro/accordion',
-	settings = {
-		apiVersion: 2,
-		title: __( 'Accordion', 'shiro-admin' ),
-		icon: 'menu',
-		category: 'wikimedia',
-		supports: {
-			align: [ 'center', 'full' ],
-		},
-		edit: () => {
-			const blockProps = useBlockProps(); // eslint-disable-line react-hooks/rules-of-hooks
+import './style.scss';
 
-			return (
-				<div { ...blockProps }>
-					<div className="accordion-wrapper">
-						<InnerBlocks
-							allowedBlocks={ [ 'shiro/accordion-item' ] }
-						/>
-					</div>
-				</div>
-			);
-		},
-		save: () => {
-			return (
+import './toggler';
+
+export const name = 'shiro/accordion';
+
+export const settings = {
+	apiVersion: 2,
+	title: __( 'Accordion', 'shiro-admin' ),
+	icon: 'menu',
+	category: 'wikimedia',
+	supports: {
+		align: [ 'center', 'full' ],
+	},
+	edit: () => {
+		const blockProps = useBlockProps(); // eslint-disable-line react-hooks/rules-of-hooks
+
+		return (
+			<div { ...blockProps }>
 				<div className="accordion-wrapper">
-					<InnerBlocks.Content />
+					<InnerBlocks
+						allowedBlocks={ [ 'shiro/accordion-item' ] }
+					/>
 				</div>
-			);
-		},
-	};
+			</div>
+		);
+	},
+	save: () => {
+		return (
+			<div className="accordion-wrapper">
+				<InnerBlocks.Content />
+			</div>
+		);
+	},
+};
