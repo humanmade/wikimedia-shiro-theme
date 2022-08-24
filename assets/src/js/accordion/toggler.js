e7b7007e8502653be60e99f69075aeae6bacd706
Pamela Ribeiro <contato@pamelanascimento.com>
2022-08-23 22:14:55 -0300

    changed function by another method

--- assets/src/js/accordion/toggler.js
+++ assets/src/js/accordion/toggler.js
@@ -3,29 +3,13 @@
  */
 
 /**
- * Find an ancestor element with a particular class.
- *
- * @param {Element} element Element to start search at.
- * @param {string} selector Class name to search for.
- * @returns {*} Ancestor element.
- */
-const findAncestor = ( element, selector ) => {
-	// eslint-disable-next-line no-cond-assign
-	while ( ( element = element.parentElement ) && ! element.classList.contains( selector ) );
-
-	return element;
-};
-
-/**
  * Toggle an accordion item open or closed.
  *
  * @param {Event} e Click event.
  */
 const toggleAccordionItem = e => {
 	e.preventDefault();
-
-	const button = e.target || e.srcElement;
-	const parent = findAncestor( button, 'accordion-item' );
+	const parent = e.target.closest( '.accordion-item' );
 
 	if ( parent.getAttribute( 'aria-expanded' ) === 'true' ) {
 		closeItem( parent );
