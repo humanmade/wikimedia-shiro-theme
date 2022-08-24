a4cdf8a57f62d0c596d0b434293246d29ec869f8
Pamela Ribeiro <contato@pamelanascimento.com>
2022-08-24 11:40:44 -0300

    Update index.js

--- assets/src/editor/blocks/collapsible-text/index.js
+++ assets/src/editor/blocks/collapsible-text/index.js
@@ -7,59 +7,73 @@
  */
  import { __ } from '@wordpress/i18n';
 
- import { InnerBlocks, useBlockProps, InspectorControls, useSetting } from '@wordpress/block-editor';
+ import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 
- import { __experimentalNumberControl as NumberControl } from '@wordpress/components';
+ import { PanelBody, RichText, SelectControl } from '@wordpress/components';
 
- import { PanelBody } from '@wordpress/components';
+ import { useState } from '@wordpress/element';
 
-
- export const name = 'shiro/collapisible-text';
+ export const name = 'shiro/collapsible-text';
 
  export const settings = {
 		 apiVersion: 2,
 		 title: __( 'Collapsible text', 'shiro-admin' ),
-		 icon: 'menu',
+		 icon: 'ellipsis',
 		 category: 'wikimedia',
+		 attributes: {
+			content: {
+				type: 'string',
+				source: 'html',
+				selector: 'h2',
+			},
+		},
 
-		 edit: ( { attributes, setAttributes } ) => {
+
+		 edit: ( { attributes, setAttributes, setFocus } ) => {
 			 const blockProps = useBlockProps(); // eslint-disable-line react-hooks/rules-of-hooks
-			 const { fontColor } = attributes;
+			 const [ size, setSize ] = useState( '50%' );
 			 return (
-				 <>
-				 <InspectorControls>
-					 <div>
-						 <p>Set how many characters:</p>
-					 </div>
-				 <PanelBody>
-				 <NumberControl
-						isShiftStepEnabled={ true }
-						onChange={ setValue }
-						shiftStep={ 10 }
-						value={ value }
+				<>
+				<InspectorControls>
+					<div>
+						<p>Set text display size:</p>
+					</div>
+					<PanelBody>
+				 	<SelectControl
+						label="Size"
+						value={ size }
+						options={ [
+							{ label: 'Big', value: '100%' },
+							{ label: 'Medium', value: '50%' },
+							{ label: 'Small', value: '25%' },
+						] }
+						onChange={ ( newSize ) => setSize( newSize ) }
 					/>
-				 </PanelBody>
-			 </InspectorControls>
-				 <div { ...blockProps }>
-					 <div className="">
-					 <RichText
-						{ ...blockProps }
-						tagName="h2" // The tag here is the element output and editable in the admin
-						value={ attributes.content } // Any existing content, either from the database or an attribute default
-						allowedFormats={ [ 'core/bold', 'core/italic' ] } // Allow the content to be made bold or italic, but do not allow other formatting options
-						onChange={ ( content ) => setAttributes( { content } ) } // Store updated content as a block attribute
-						placeholder={ __( 'Heading...' ) } // Display this text before any content has been added by the user
-					/>
-					 </div>
-				 </div>
-				 </>
+					</PanelBody>
+			 	</InspectorControls>
+					<div { ...blockProps }>
+						<div className="">
+							<RichText
+								className="accordion-item__title-text"
+								formattingControls={ [] }
+								placeholder={
+									'Add Accordion Title...'
+								}
+								tagName="h3"
+								value={ attributes.title }
+								onChange={ content => setAttributes( { title: content } ) }
+								onFocus={ setFocus }
+							></RichText>
+						</div>
+					</div>
+				</>
 			 );
 		 },
-		 save: () => {
+		 save: ( attributes ) => {
 			 return (
 				 <div>
 					 <div>
-					 <RichText.Content { ...blockProps } tagName="h2" value={ attributes.content } />
+						<RichText.Content tagName="h2" value={ attributes.content } />
 					 </div>
 				 </div>
 			 );
